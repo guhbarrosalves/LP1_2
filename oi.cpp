b069d845a1d7aa17ab63aa91419423a16dee3b9c
Gustavo Barros <guhbarros.alves@gmail.com>
2026-03-31 07:59:33 -0300

    filter

--- oi.cpp
+++ oi.cpp
@@ -1,15 +1,28 @@
 #include <iostream>
+#include <algorithm>
+#include <array>
+#include <iterator>
+#include <functional>
+
+int *filter(int *first, int *last){
+    auto *slow{first};
+    auto *fast{first};
+    while(fast != last){
+        if(*fast > 0){
+            std::iter_swap(fast, slow);
+            slow++;
+        }
+        fast++;
+    }
+    
+    return last;
+}
+
 
 int main(){
-    int idade;
+    std::array v1 = {1, 2, -1, -2, 3, 5, -8, 4};
 
-    std::cout << "Isso é uma alteração\n";
 
-    std::cout << "Digite sua idade:\n";
-    
-    std::cin >> idade;
-    
-    std::cout << "Mais um teste\n";
 
     return 0;
 }
